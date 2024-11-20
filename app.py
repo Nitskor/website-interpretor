781ef1962e80364d963e4082e8a37e8aff0eca48
Nitskor <koruthunitin@gmail.com>
2024-11-20 18:38:58 +0530

    data loader with clean text in app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask,render_template
+from chains import Chain
+from langchain_community.document_loaders import WebBaseLoader
+from utils import clean_text
 
 app = Flask(__name__)
 
+loader = WebBaseLoader("https://jobs.nike.com/?jobSearch=true&jsOffset=0&jsSort=posting_start_date&jsLanguage=en&error=jobPost")
+page_data = clean_text(loader.load().pop().page_content)
+
+
 @app.route('/')
 def hello_world():    
-    return render_template('home.html')
+    return page_data
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
+
