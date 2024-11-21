1ad0294fdf3fff90fc33246f9c43e36a826f0f39
Nitskor <koruthunitin@gmail.com>
2024-11-21 18:39:42 +0530

    output.html, conected, chain to submit button

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from chains import Chain
 from langchain_community.document_loaders import WebBaseLoader
 from utils import clean_text
 
 app = Flask(__name__)
 
-loader = WebBaseLoader("https://jobs.nike.com/?jobSearch=true&jsOffset=0&jsSort=posting_start_date&jsLanguage=en&error=jobPost")
-page_data = clean_text(loader.load().pop().page_content)
+#loader = WebBaseLoader("https://jobs.nike.com/?jobSearch=true&jsOffset=0&jsSort=posting_start_date&jsLanguage=en&error=jobPost")
+#page_data = clean_text(loader.load().pop().page_content)
 
 chain = Chain()
-try:
-    jobs = chain.extract_jobs(page_data)
-except Exception as e:
-    raise f"Error: {e}"
+#try:
+    #jobs = chain.extract_jobs(page_data)
+#except Exception as e:
+    #raise f"Error: {e}"
 
 @app.route('/')
 def hello_world():    
-    return render_template('home.html',jobs=jobs)
+    return render_template('home.html')
+
+@app.route('/output' ,methods=['POST'])
+def output():
+    data = str(request.form.get('url'))
+    loader = WebBaseLoader(data)
+    page_data = clean_text(loader.load().pop().page_content)
+    jobs = chain.extract_jobs(page_data)    
+    return render_template('output.html',jobs=jobs,data=data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
