aae2c8a58d53b77c7a52ccd3ef7b3d09963475dc
Nitskor <koruthunitin@gmail.com>
2024-11-22 12:00:14 +0530

    Email generator done with a display of the jobs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request
 from chains import Chain
 from langchain_community.document_loaders import WebBaseLoader
 from utils import clean_text
+from portfolio import Portfolio
 
 app = Flask(__name__)
 
@@ -9,10 +10,15 @@ app = Flask(__name__)
 #page_data = clean_text(loader.load().pop().page_content)
 
 chain = Chain()
+portfolio = Portfolio()
+portfolio.load_portfolio()
 #try:
     #jobs = chain.extract_jobs(page_data)
 #except Exception as e:
     #raise f"Error: {e}"
+#skills = ['python']
+#link = portfolio.query_links(skills=skills)
+#mail = chain.write_mail(job=jobs,links=link)
 
 @app.route('/')
 def hello_world():    
@@ -23,8 +29,11 @@ def output():
     data = str(request.form.get('url'))
     loader = WebBaseLoader(data)
     page_data = clean_text(loader.load().pop().page_content)
-    jobs = chain.extract_jobs(page_data)    
-    return render_template('output.html',jobs=jobs,data=data)
+    jobs = chain.extract_jobs(page_data)
+    skills = 'python'
+    link = portfolio.query_links(skills=skills)
+    mail = chain.write_mail(jobs,link)  
+    return render_template('output.html',jobs=jobs,data=data,mail=mail.content)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- chains.py
+++ chains.py
@@ -47,7 +47,7 @@ class Chain:
         Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ
         in fulfilling their needs.
         Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-        Remember you are Mohan, BDE at AtliQ.
+        Remember you are Nitin, BDE at AtliQ.
         Do not provide a preamble.
         ### EMAIL (NO PREAMBLE):
 
@@ -55,3 +55,4 @@ class Chain:
         )
     chain_email = prompt_email | self.llm
     res = chain_email.invoke({"job_description": str(job), "link_list": links})
+    return res
